7aee62ffd00d95251fe7d258796dcbfd0709b203
bed shah <sbed19@tbc.edu.np>
2023-03-19 22:16:26 +0545

    lab2 completed

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tipcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int var1=0;
+        if(var1==0){
+
+
+        }
+    }
+}
